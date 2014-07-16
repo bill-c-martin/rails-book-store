284ce467a2fb95a0b2f0a0bbe8078def9911b915
William Martin <bitterpeace@gmail.com>
2014-07-15 22:08:21 -0400

    added basical functional tests

--- test/functional/store_controller_test.rb
+++ test/functional/store_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class StoreControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4 # 4 links in left col
+    assert_select '#main .entry', 3              # 3 products display
+    assert_select 'h3', 'Programming Ruby 1.9'   # Verify a title
+    assert_select '.price', /\$[,\d]+\.\d\d/     # Verify a price exists with format 123,123.45
   end
 
 end
