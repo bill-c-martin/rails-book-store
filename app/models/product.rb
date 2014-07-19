1a208e10297c9980398edb58951997599eb21bf7
William Martin <bitterpeace@gmail.com>
2014-07-19 01:34:33 -0400

    setup product to have many line items, as well as destroy validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   attr_accessible :description, :image_url, :price, :title
 
   validates :title, :description, :image_url, presence: true
@@ -9,4 +12,14 @@ class Product < ActiveRecord::Base
     with:   %r{\.(gif|jpg|png)\Z}i,
     message: 'image must be gif, png, or jpg'
   }
+
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base, 'Line Items Present')
+        return false
+      end
+    end
 end
