fb927c3dbbc16e520d998cdcd55a8faef35842a4
Bill Martin <bitterpeace@gmail.com>
2014-09-14 18:43:15 -0400

    Create migration that creates line item price column and copies product price to it

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,6 +2,7 @@ class LineItem < ActiveRecord::Base
   belongs_to :product
   belongs_to :cart
   attr_accessible :cart_id, :product_id
+  attr_accessor :price
 
   def total_price
     product.price * quantity
