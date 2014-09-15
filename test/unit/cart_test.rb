bd56a5869444ac09ed60efec9451649fcf18e8b3
Bill Martin <bitterpeace@gmail.com>
2014-09-14 23:09:21 -0400

    Write unit tests for cart model to test unique and duplicate products being added to the cart

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
+  fixtures :products
+
   # test "the truth" do
   #   assert true
   # end
+
+  test "Cart should create a new line item when adding a product" do
+    cart = Cart.new
+    line_item = cart.add_product(products(:one).id)
+    assert_equal 1, cart.line_items.count
+  end
+
+  test "Cart should create two line items when adding two different products" do
+    cart = Cart.new
+    line_item = cart.add_product(products(:one).id)
+    line_item2 = cart.add_product(products(:ruby).id)
+    assert_equal 2, cart.line_items.count
+  end
+
+  test "Cart should create only one line item when adding a product more than once" do
+    cart = Cart.new
+    line_item = cart.add_product(products(:one).id)
+    line_item = cart.add_product(products(:one).id)
+    assert_equal 1, cart.line_items.count
+  end
+
+  test "Cart should create two line items when adding two products any number of times" do
+    cart = Cart.new
+    line_item = cart.add_product(products(:one).id)
+    line_item = cart.add_product(products(:ruby).id)
+    assert_equal 2, cart.line_items.count
+
+    line_item = cart.add_product(products(:ruby).id)
+    line_item = cart.add_product(products(:one).id)
+    line_item = cart.add_product(products(:ruby).id)
+    assert_equal 2, cart.line_items.count
+  end
 end
