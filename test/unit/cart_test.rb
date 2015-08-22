f5a8a7e9ee282cbc3964939261999ca99bb9a97c
Bill Martin <bitterpeace@gmail.com>
2015-08-22 03:08:37 -0400

    Add unit test line item price checking when a product is added to the cart

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -6,7 +6,6 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
-
   test "Cart should create a new line item when adding a product" do
     cart = Cart.new
     cart.add_product(products(:gameOfThrones).id)
@@ -36,5 +35,15 @@ class CartTest < ActiveSupport::TestCase
     cart.add_product(products(:catsCradle).id)
     cart.add_product(products(:catsCradle).id)
     assert_equal 2, cart.line_items.size
+  end
+
+  test "Cart should store a product's current price as a line item price when the product is added to the cart" do
+    cart = Cart.new
+    cart.add_product(products(:gameOfThrones).id)
+    cart.add_product(products(:catsCradle).id)
+    cart.add_product(products(:theNameOfTheWind).id)
+    cart.line_items.each do |line_item|
+      assert_equal line_item.product.price, line_item.price
+    end
   end
 end
