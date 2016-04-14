de5a6ea87d030c5e404128c9e625cb5a09e70c73
Bill Martin <bill@billmartin.io>
2016-04-13 23:39:25 -0400

    Setup relationships between orders and line items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ActiveRecord::Base
+  has_many :line_items, dependent: :destroy
+
   attr_accessible :address, :email, :name, :pay_type
   PAYMENT_TYPES = [ "Check", "Credit Card", "Purchase Order" ]
 
