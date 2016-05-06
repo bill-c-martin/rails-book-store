9ef8aa261ee99c9f1600e15b302a606c281382f6
Bill Martin <bill@billmartin.io>
2016-05-05 23:39:21 -0400

    Create functional tests for order received and shipped notifications

--- test/functional/order_notifier_test.rb
+++ test/functional/order_notifier_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderNotifierTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderNotifier.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.received(orders(:one))
+    assert_equal "Readiac Order Confirmation", mail.subject
+    assert_equal ["bitterpeace@gmail.com"], mail.to
+    assert_equal ["bitterpeace@gmail.com"], mail.from
+    assert_match /1 x A Game of Thrones/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Readiac Order Shipped", mail.subject
+    assert_equal ["bitterpeace@gmail.com"], mail.to
+    assert_equal ["bitterpeace@gmail.com"], mail.from
+    assert_match /1\sx\sA Game of Thrones/, mail.body.encoded
   end
 
 end
