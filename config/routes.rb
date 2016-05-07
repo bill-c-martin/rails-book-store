1a1582ea4293016617dc469e1adf7b0fec5b1ed6
Bill Martin <bill@billmartin.io>
2016-05-07 12:21:46 -0400

    Setup public-facing controllers to support locales being passed in the URL like /en or /es, only English is setup at this time

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,25 @@
 Depot::Application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get 'login' => :new
     post 'login' => :create
     delete 'logout' => :destroy
   end
-  resources :users
-  resources :orders
-  resources :carts
-  resources :line_items do
-    member do 
-      post 'decrement'
-    end
-  end
-  resources :products do
-    get :who_bought, on:  :member
-  end
 
-  root to: 'store#index', as: 'store'
+  scope '(:locale)' do
+    resources :users
+    resources :orders
+    resources :carts
+    resources :line_items do
+      member do
+        post 'decrement'
+      end
+    end
+    resources :products do
+      get :who_bought, on:  :member
+    end
+
+    root to: 'store#index', as: 'store'
+  end
 end
