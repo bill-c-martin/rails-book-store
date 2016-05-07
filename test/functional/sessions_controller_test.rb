d458016649267dcf22567c293490f385d7fd3abe
Bill Martin <bill@billmartin.io>
2016-05-07 00:05:08 -0400

    Write functional tests to ensure system handles successful & unsuccessful logins properly, and logouts

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should get create" do
-    get :create
-    assert_response :success
+  test 'should login' do
+    test_user = users(:one)
+    post :create, name: test_user.name, password: 'test'
+    assert_redirected_to admin_url
+    assert_equal test_user.id, session[:user_id]
   end
 
-  test "should get destroy" do
-    get :destroy
-    assert_response :success
+  test 'should fail login' do
+    test_user = users(:one)
+    post :create, name: test_user.name, password: 'wrong'
+    assert_redirected_to login_url
   end
 
+  test 'should logout' do
+    delete :destroy
+    assert_redirected_to store_url
+  end
 end
